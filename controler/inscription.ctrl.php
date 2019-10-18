9d56a5c7462fe9292b303f1d96ff50ae3fca246b
BaptSeg <segeat.b@gmail.com>
2019-10-18 19:23:42 +0200

    Commit

--- controler/inscription.ctrl.php
+++ controler/inscription.ctrl.php
@@ -1,16 +1,19 @@
 <?php
-include_once("../view/inscription.view.php");
 // Vérification de la validité des informations
 
 $bdd = new PDO('sqlite:../data/utilisateur.db');
 
+echo "ok1";
 if (isset($_POST['inscription'])) {
+  echo "ok2";
   $reponse = $bdd->query('SELECT pseudo FROM Utilisateur WHERE pseudo = "'.$_POST['pseudo'].'"');
   $pseudo = $reponse->fetch();
   if (mb_strtolower($pseudo) != mb_strtolower($_POST['pseudo'])) {
+    $reponse = $bdd->query("SELECT max(id) FROM Utilisateur");
+    $max = $reponse->fetch();
     $req = $bdd->prepare("INSERT INTO Utilisateur(id, pseudo, mdp, email, date_inscription) VALUES(:id, :pseudo, :mdp, :email, :date_inscription)");
     $req->execute(array(
-      "id" => 1,
+      "id" => $max[0]+1,
       "pseudo" => $_POST['pseudo'],
       "mdp" => $_POST['mdp1'],
       "email" => $_POST['email'],
@@ -32,5 +35,7 @@ if (isset($_POST['inscription'])) {
 //     'pseudo' => $pseudo,
 //     'pass' => $pass_hache,
 //     'email' => $email));
+
+include("../view/inscription.view.php");
 
 ?>
